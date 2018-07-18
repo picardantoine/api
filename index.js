0e57911ff692e84382b00aa24c0995b543b829a6
antoine picard <antn.picard@gmail.com>
2018-07-18 14:46:57 +0200

    created a hello world in terminal

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.get('/', function (req, res) {
-    var mess= req.query['message'];
-  res.send(mess)
-})
+// app.get('/', function (req, res) {
+//     var mess= req.query['message'];
+//   res.send(mess)
+// })
+
+// // dans mon local host je peux taper : http://localhost:5000/?message=blabla
 
 
+// app.listen(5000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 3000!')
-})
+console.log('hello world!');  
